72f022523b30c0f5459cc0bc6d172558a40badb3
Sahil Singh <sahilsinghat12w12@gmail.com>
2026-07-10 16:23:38 +0530

    Added debugging for Streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,22 +9,27 @@ petal_length = st.number_input("Petal Length")
 petal_width = st.number_input("Petal Width")
 
 if st.button("Predict"):
+    try:
+        response = requests.post(
+            "https://iris-project-1-5rlt.onrender.com/predict",
+            json={
+                "sepal_length": sepal_length,
+                "sepal_width": sepal_width,
+                "petal_length": petal_length,
+                "petal_width": petal_width
+            },
+            timeout=90
+        )
 
-    response = requests.post(
-        "https://iris-project-1-5rlt.onrender.com//predict",
-        json={
-            "sepal_length": sepal_length,
-            "sepal_width": sepal_width,
-            "petal_length": petal_length,
-            "petal_width": petal_width
-        }
-    )
-    result = response.json()
+        st.write("Status Code:", response.status_code)
+        st.write("Response:", response.text)
 
-    if response.status_code == 200:
-        result = response.json()
-        st.success(result["prediction"])
-    else:
-        st.error("Prediction failed!")
+        if response.status_code == 200:
+            st.success(response.json()["prediction"])
+        else:
+            st.error("Prediction failed!")
+
+    except Exception as e:
+        st.error(e)
 
     
